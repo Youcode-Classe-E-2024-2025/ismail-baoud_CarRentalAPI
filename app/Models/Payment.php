2f28fe9ebe29b2ad29fda8f27eaf4daffba5ef2f
ismailbaoud <ibaoud46@gmail.com>
2025-03-13 14:22:08 +0000

    ajouter les models

---
+++ app/Models/Payment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['rental_id', 'amount', 'method', 'status'];
+
+    public function rental()
+    {
+        return $this->belongsTo(Rental::class);
+    }
+}
